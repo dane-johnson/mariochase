9caccff5bf59b87e21ed5721262edb26b942fc45
Dane Johnson <daneallenjohnson@protonmail.com>
2020-08-21 17:31:49 -0500

    Fix dutch angle bug

--- GarrysMod/gamemodes/mariochase/gamemode/cl_init.lua
+++ GarrysMod/gamemodes/mariochase/gamemode/cl_init.lua
@@ -4,6 +4,9 @@ include("cl_hud.lua")
 hook.Add(
    "CalcView", "MyCalcView",
    function( ply, pos, angles, fov )
+      -- Fix the camera getting dutched
+      local pitch, yaw = angles:Unpack()
+      angles:SetUnpacked(pitch, yaw, 0)
       local view = {
          origin = pos - ( angles:Forward() * 100 ),
          angles = angles,
